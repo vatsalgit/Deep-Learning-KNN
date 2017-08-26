e7729dd82071c53bc3994c68328952bb07ec9a7a
Vatsal <vtsl.shh@gmail.com>
2017-08-25 21:51:18 -0700

    End to end first

--- 7996627075.py
+++ 7996627075.py
@@ -1,6 +1,8 @@
 import sys
-import os
+import numpy as np
 import pickle
+from sklearn.decomposition import PCA
+
 
 class Dlknn (object):
 
@@ -10,7 +12,6 @@ class Dlknn (object):
         self.D = D
         self.PATH_TO_DATA = PATH_TO_DATA
 
-
     def load_images(self, num=1000):
         with open(self.PATH_TO_DATA, 'rb') as fo:
             my_dict = pickle.load(fo, encoding='bytes')
@@ -18,16 +19,64 @@ class Dlknn (object):
         image_data = my_dict[b'data'][:num]
         return labels, image_data
 
+    def train_test_split(self):
+        labels, image_data = self.load_images()
+        train_X, train_y = image_data[self.N:], labels[self.N:]
+        test_X, test_y = image_data[:self.N], labels[:self.N]
+        return train_X, train_y, test_X, test_y
+
+    @staticmethod
+    def convert_to_grayscale(X):
+        new_image = []
+        for image in X:
+            R , G, B = image[:1024], image[1025:2049], image[2048:]
+            gray = (0.299 * R) + (0.587 * G) + (0.114 * B)
+            new_image.append(gray)
+        return new_image
+
+    def grayscale_transform(self):
+        train_X, train_y, test_X, test_y = self.train_test_split()
+        train_X = self.convert_to_grayscale(train_X)
+        test_X = self.convert_to_grayscale(test_X)
+        return np.array(train_X), np.array(train_y), test_X, test_y
+
+    def pca_fit(self, X):
+        pca = PCA(n_components=self.D, svd_solver='full')
+        self.fitted_pca = pca.fit(X)
+
+    def pca_transform(self, X):
+        new_X = self.fitted_pca.transform(X)
+        return new_X
+
+    def calculate_inverse_euclidean(self, train_X, test_image, train_y):
+        distances = [(np.sum(1/abs(test_image-n[0])**2) , n[1]) for n in zip(train_X, train_y) ]
+        distances = sorted(distances, key=lambda x:x[0], reverse=True)[:self.K]
+        return distances
+
+    def predict(self, train_X, test_X, train_y, test_y):
+        all_image_distances = [(self.calculate_inverse_euclidean(train_X, test_image, train_y), label) for test_image,label in zip(test_X, test_y) ]
+        myfile = open('7996627075.txt', 'w')
+        for image, true_label in all_image_distances:
+            my_dict = {}
+            for distance,label in image:
+                my_dict[label] = my_dict.get(label, 0 ) + distance
+            predicted_label = max(my_dict, key=my_dict.get)
+            output_string = str(predicted_label) + ' ' + str(true_label) +'\n'
+            myfile.write(output_string)
+        myfile.close()
 
 def load_args(args='sys'):
     if args == 'sys':
         return sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
     else:
-        return 3, 2, 100, "C:/Users/vatsal/OneDrive/Deep Learning/Data/data_batch_1"
+        return 5, 100, 10, "C:/Users/vatsal/OneDrive/Deep Learning/Data/data_batch_1"
 
 
 if __name__ == '__main__':
     k, d, n, PATH_TO_DATA = load_args('default')
     knn = Dlknn(k, d, n, PATH_TO_DATA)
-    labels, image_data = knn.load_images()
-    main()
+    train_X, train_y, test_X, test_y = knn.grayscale_transform()
+    knn.pca_fit(train_X)
+    pca_train = knn.pca_transform(train_X)
+    pca_test = knn.pca_transform(test_X)
+    knn.predict(pca_train, pca_test, train_y, test_y)
